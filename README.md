16697e38cd79e831f450ca3e54defd2a43a5f215
qiangzai <3486105329@qq.com>
2023-02-12 10:57:40 +0800

    firstCommit

--- README.md
+++
@@ -1,2 +0,0 @@
-# springcloud-dubbo
-a simple demo of deubbo
---
+++ producer-server/src/main/java/com/lv/ProducerServerApplication.java
@@ -0,0 +1,16 @@
+package com.lv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 17324
+ */
+@SpringBootApplication
+public class ProducerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerServerApplication.class, args);
+	}
+
+}
